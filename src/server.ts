d2a706bb89639befe50204e0366f4cb5ce642161
Ardeshir Saadat <ardeshirsaadat@yahoo.com>
2022-05-31 12:15:25 -0400

    Modify build scripts

--- src/server.ts
+++ src/server.ts
@@ -28,30 +28,15 @@ function isValidHttpUrl(url_string:string) {
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
   app.get('/filteredimage',async (req,res)=>{
     let {image_url} = req.query;
     let bool = isValidHttpUrl(image_url)
   
     if (bool){
       try{
-        console.log(image_url)
       const filterd_image_file = await filterImageFromURL(image_url)
- 
+      console.log(filterd_image_file)
       res.status(200).sendFile(filterd_image_file,()=>{
         deleteLocalFiles([filterd_image_file])
       })
@@ -63,7 +48,7 @@ function isValidHttpUrl(url_string:string) {
     res.status(422).send({error:"query parameters",message:"url is malformed"})
     }
   })
-  //! END @TODO1
+  
   
   // Root Endpoint
   // Displays a simple message to the user
